2b34bfebeddb846f8f656264bdedd6fb1da620b7
Nezzei <magnus@reso.dk>
2021-09-24 14:03:13 +0200

    Add Dymoprint

---
+++ Move2x/DymoPrint/main.py
@@ -0,0 +1,25 @@
+from win32com.client import Dispatch
+import pathlib
+
+label_val = 'customer name'
+label_product = 'product name'
+my_printer = 'DYMO LabelWriter 450 Turbo'
+label_path = pathlib.Path('move2xLabel.label')
+
+printer_com = Dispatch('Dymo.DymoAddIn')
+printer_com.SelectPrinter('my_printer')
+printer_com.Open(label_path)
+
+printer_label = Dispatch("Dymo.DymoLabels")
+printer_label.SetField('TEXT', label_product)
+printer_label.SetField('ADDRESS', label_val)
+
+printer_com.StartprintJob()
+printer_com.Print(3, False)
+printer_com.EndPrintJob()
+
+
+
+
+
+
