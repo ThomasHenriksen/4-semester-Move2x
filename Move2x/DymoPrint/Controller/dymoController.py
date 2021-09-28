5926f2f6cdd18226dfd690f067b63958101bbdfb
Nezzei <magnus@reso.dk>
2021-09-28 12:00:50 +0200

    dymoController & dymoModel
    Controller and Model  layers created but needs codeing

---
+++ Move2x/DymoPrint/Controller/dymoController.py
@@ -0,0 +1,20 @@
+from win32com.client import Dispatch
+import pathlib
+
+my_printer = 'DYMO LabelWriter 450 Turbo'
+label_path = pathlib.Path('move2xLabel.label')
+
+class dymoController(object):
+    """description of class"""
+
+    printer_com = Dispatch('Dymo.DymoAddIn')
+    printer_com.SelectPrinter('my_printer')
+    printer_com.Open(label_path)
+
+    printer_label = Dispatch("Dymo.DymoLabels")
+    printer_label.SetField('TEXT', label_product)
+    printer_label.SetField('ADDRESS', label_val)
+
+    printer_com.StartprintJob()
+    printer_com.Print(1, False)
+    printer_com.EndPrintJob()
---
+++ Move2x/DymoPrint/Model/dymoModel.py
@@ -0,0 +1,5 @@
+class dymoModel(object):
+    """description of class"""
+    label_val = ''.
+    label_product = 'product name'
+
