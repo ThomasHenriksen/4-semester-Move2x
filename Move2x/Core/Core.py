f773d9bcaa0797d5b175100cf45c8fee0f90d825
ThomasHenriksen <1083787@ucn.dk>
2021-09-28 13:59:12 +0200

    Core
    Import ImageReader

--- Move2x/Core/Core.py
+++ Move2x/Core/Core.py
@@ -1,1 +1,9 @@
+import sys as ir
+import sys as dp
+ir.path.append('..\ImageReader')
+from service import imageReaderService as service
+path = 'order2.jpg' 
+list = service.OCR(path)
 
+for b in list:
+    print(b.customerNO)
