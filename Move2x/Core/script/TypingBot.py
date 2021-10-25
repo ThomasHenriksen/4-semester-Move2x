feb1820948c201ca8c72cdffaacbdbff510b7440
ThomasHenriksen <70630520+ThomasHenriksen@users.noreply.github.com>
2021-10-25 10:08:05 +0200

    Update TypingBot.py

--- Move2x/Core/script/TypingBot.py
+++ Move2x/Core/script/TypingBot.py
@@ -8,9 +8,7 @@ import os
 
 def type_string_with_delay(text):
     chooseword = text
-    print(chooseword)
     for character in chooseword:  # Loop over each character in the string
-       
        pyautogui.press(character)
        time.sleep(0.05)  # Sleep for the amount of seconds generated
 
