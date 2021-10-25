b762fee2216c6b8a5593145fcac7206527f70387
ThomasHenriksen <70630520+ThomasHenriksen@users.noreply.github.com>
2021-10-25 10:41:29 +0200

    Update TypingBot.py

--- Move2x/Core/script/TypingBot.py
+++ Move2x/Core/script/TypingBot.py
@@ -6,16 +6,12 @@ import os
 
 
 
-def type_string_with_delay(text):
-    
+def type_string_with_delay(text):   
     word = text[0]
-    
-    
-    time.sleep(0.2)
     for character in word:  # Loop over each character in the string
        print(character)
        pyautogui.press(character)
-       time.sleep(0.50)  # Sleep for the amount of seconds generated
+       time.sleep(0.05)  # Sleep for the amount of seconds generated
 
 def printLabel():
     pyautogui.keyDown('ctrl')
