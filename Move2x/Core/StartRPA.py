06f8bfd2d2f82be471e59d187e3dc2d3a5e01a6f
ThomasHenriksen <70630520+ThomasHenriksen@users.noreply.github.com>
2021-10-25 08:42:28 +0200

    hhj

--- Move2x/Core/StartRPA.py
+++ Move2x/Core/StartRPA.py
@@ -8,13 +8,15 @@ win = Tk()
 win.geometry("200x100")
 
 def run():
+    label.config(text="is running")
     move.main()
+    label.config(text="is not running")
     #creating a Label
-label = Label(win,  text="unchanged")
+label = Label(win,  text="is not running")
 label.pack()
 sub_btn=tk.Button(win,text = 'Run', command = run)
 sub_btn.pack()
-label.config(text="changed")
+
 
 
 win.mainloop()
