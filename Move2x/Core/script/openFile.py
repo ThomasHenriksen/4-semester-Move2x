b37f2db10699f2627d1fb9fb7514bdbc6692c304
ThomasHenriksen <1083787@ucn.dk>
2021-10-26 15:23:51 +0200

    Update openFile.py

--- Move2x/Core/script/openFile.py
+++ Move2x/Core/script/openFile.py
@@ -2,7 +2,7 @@ import subprocess
 import shutil
 import os
 from pathlib import Path
-
+import time
 def openDymo():
     path_to_notepad = 'C:\Program Files (x86)\DYMO\DYMO Label Software\DLS.exe'
     path_to_file = 'resources\\Label.label'
@@ -14,9 +14,34 @@ def deleteFile():
         os.remove("resources\\Label.label")
 
 def copyFile():
-    src = r'resources\\newBackUp.label'
-    dst =  r'resources\\Label.label'
-    shutil.copyfile(src, dst)
+    # Source path
+    source = "/resources/newBackUp.label"
+ 
+    # Destination path
+    destination = "/resources/Label.label"
+ 
+    # Copy the content of
+    # source to destination
+ 
+    try:
+        shutil.copyfile(source, destination)
+        print("File copied successfully.")
+ 
+    # If source and destination are same
+    except shutil.SameFileError:
+        print("Source and destination represents the same file.")
+ 
+    # If destination is a directory.
+    except IsADirectoryError:
+        print("Destination is a directory.")
+ 
+    # If there is any permission issue
+    except PermissionError:
+        print("Permission denied.")
+ 
+    # For other errors
+    except:
+        print("Error occurred while copying file.")
 
 copyFile()
 openDymo()
