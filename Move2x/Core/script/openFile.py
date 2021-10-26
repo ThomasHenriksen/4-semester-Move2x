8f933995c6e62670f5953f9832546988b731e148
ThomasHenriksen <1083787@ucn.dk>
2021-10-26 15:14:17 +0200

    Update openFile.py

--- Move2x/Core/script/openFile.py
+++ Move2x/Core/script/openFile.py
@@ -4,18 +4,20 @@ import os
 from pathlib import Path
 
 def openDymo():
+    path_to_notepad = 'C:\Program Files (x86)\DYMO\DYMO Label Software\DLS.exe'
+    path_to_file = 'resources\\Label.label'
+    subprocess.call([path_to_notepad, path_to_file])
+
+def deleteFile():
     my_filePNG = Path("resources\\Label.label")
     if my_filePNG.is_file():
         os.remove("resources\\Label.label")
-   
+
+def copyFile():
     src = r'resources\\newBackUp.label'
     dst =  r'resources\\Label.label'
     shutil.copyfile(src, dst)
 
-
-    path_to_notepad = 'C:\Program Files (x86)\DYMO\DYMO Label Software\DLS.exe'
-    path_to_file = 'resources\\Label.label'
-
-    subprocess.call([path_to_notepad, path_to_file])
-
+copyFile()
 openDymo()
+deleteFile()
