8a1ea11bab711ff2b454460098e7c752244411f9
Nezzei <60960063+Nezzei@users.noreply.github.com>
2021-11-16 22:16:00 +0100

    Update GUI ikke færdigt

--- Move2x/Move2x - RPA Ver 0.5/script/LabelGUI.py
+++ Move2x/Move2x - RPA Ver 0.5/script/LabelGUI.py
@@ -1,7 +1,10 @@
 import tkinter as tk
+from tkinter import *
 from concurrent import futures
 import time
 import os 
+from PIL import Image, ImageTk
+from pathlib import Path
 
 thread_pool_executor = futures.ThreadPoolExecutor(max_workers=1)
  
@@ -10,16 +13,26 @@ class MainFrame(tk.Frame):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.label = tk.Label(self, text='not running')
-        self.label.pack()
+        self.label.pack(side="top", pady=5, anchor="nw")
         self.listbox = tk.Listbox(self)
-        self.listbox.pack()
+        self.listbox.pack(side="left", padx=20, pady=20, anchor="n")
         self.buttonDymo = tk.Button(
             self, text='Dymo Print', command=self.btnDymo)
-        self.buttonDymo.pack(pady=5)
+        self.buttonDymo.pack( anchor="s", side="left", pady=5)
         self.buttonScanner = tk.Button(
             self, text='Scan order', command=self.btnWebcam)
-        self.buttonScanner.pack(pady=5)
-        self.pack()
+        self.buttonScanner.pack( anchor="s",side="left", pady=5,)
+        backgroundImg = ImageTk.PhotoImage(Image.open("resources\\Label.png").resize((341, 198)))
+        self.lblBgImg=tk.Label(self, image = backgroundImg)
+        self.lblBgImg.image = backgroundImg
+        self.lblBgImg.pack(side="top")
+        self.lblCustomer = tk.Label(self, bg="gray", text='test')
+        self.lblCustomer.pack(side="top", pady=20, padx= 50)
+        self.lblTime = tk.Label(self, bg="gray", text='test på hvor meget der kan være af text i et label')
+        self.lblTime.place(relx=0.5, rely=0.5, anchor='s')
+        self.lblProduct = tk.Label(self, text='Some Plain Text', compound='center')
+        self.lblProduct.pack(pady=59, anchor="n")
+        self.pack(fill='none', expand=1)
  
     def btnDymo(self):
         
@@ -60,5 +73,6 @@ class MainFrame(tk.Frame):
  
 if __name__ == '__main__':
     app = tk.Tk()
+    app.geometry("650x350")
     main_frame = MainFrame()
     app.mainloop()
