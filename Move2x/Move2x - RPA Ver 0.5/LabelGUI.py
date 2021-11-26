b5db8e68cc735efff69fd0b3455390bad8a52f3f
ThomasHenriksen <1083787@ucn.dk>
2021-11-26 09:37:03 +0100

    Update LabelGUI.py

--- Move2x/Move2x - RPA Ver 0.5/LabelGUI.py
+++ Move2x/Move2x - RPA Ver 0.5/LabelGUI.py
@@ -12,16 +12,21 @@ class MainFrame(tk.Frame):
  
     def __init__(self, *args, **kwargs):
         btnY = 265
+
         super().__init__(*args, **kwargs)
-        self.label = tk.Label(self, text='')
-        self.label.pack(side="top", pady=5, padx=5, anchor="w")
+
+        
+
         sb = Scrollbar(self, orient=VERTICAL)
         sb.pack(side=RIGHT, fill=Y)
+
         self.listbox = tk.Listbox(self)
         self.listbox.bind('<<ListboxSelect>>', self.getElement)
         self.listbox.configure(yscrollcommand=sb.set)
+
         sb.config(command=self.listbox.yview)
         self.listbox.pack(side="left", padx=0, pady=0, anchor="nw", fill=tk.X,expand=True)
+
         backgroundImg = ImageTk.PhotoImage(Image.open("resources\\Label.png").resize((341, 198)))
         self.lblBgImg=tk.Label(self, image = backgroundImg)
         self.lblBgImg.image = backgroundImg
@@ -31,13 +36,16 @@ class MainFrame(tk.Frame):
         self.btnCancel.place(y=btnY, x=300, anchor='s')
         self.btnPrint = tk.Button(self, text='Print Order', command=self.btnPrint)
         self.btnPrint.place(y=btnY, x=400, anchor='s')
-       
+        self.btnRefresh = tk.Button(self, text='Refresh Orders', command=self.btnRefresh)
+        self.btnRefresh.place(y=btnY, x=190, anchor='s')
+
         self.lblCustomer = tk.Label(self, bg="white", text='Customer')
-        self.lblCustomer.place(y=112, x=430, anchor='s') # y=120, x=470, anchor='s'
+        self.lblCustomer.place(y=82, x=430, anchor='s') # y=120, x=470, anchor='s'
         self.lblProduct = tk.Label(self, bg="white", text=' ')
-        self.lblProduct.place(y=141, x=420, anchor='s')
+        self.lblProduct.place(y=111, x=420, anchor='s')
         self.lblTime = tk.Label(self, bg="white", text='time', compound='center')
-        self.lblTime.place(y=112, x=515, anchor='s')
+        self.lblTime.place(y=82, x=515, anchor='s')
+
         self.options = tk.StringVar(self) # variable 
         global qualityList 
         qualityList = ["1"]
@@ -54,20 +62,30 @@ class MainFrame(tk.Frame):
         self.xmlOrder()
 
     def btnCancel(self):
-        
         thread_pool_executor.submit(self.blocking_Cancel)
+
     def btnPrint(self):
-        
         thread_pool_executor.submit(self.blocking_Print)
+    def btnRefresh(self):
+        thread_pool_executor.submit(self.blocking_refresh)
     def xmlOrder(self):
-         orderList = xml.getOrder()
-         for order in orderList:
-             self.after(0, self.listbox_insert, order[1] +' '+ order[4] )
-         self.after(0, self.set_lblCustomer_text, orderList[0][1])
-         self.after(0, self.set_lblTime_text, orderList[0][2])
-         self.after(0, self.set_size_options(orderList[0][3]))
-         self.after(0, self.set_lblProduct_text, orderList[0][4])
-         return orderList
+        self.listbox.delete(0,tk.END)
+        orderList = xml.getOrder()
+        for order in orderList:
+            if(order[1] == '000000'):
+                self.btnCancel['state'] = 'disabled'
+                self.btnPrint['state'] = 'disabled'
+                self.om1['state'] = 'disabled'
+            else:
+                self.btnCancel['state'] = 'normal'
+                self.btnPrint['state'] = 'normal'
+                self.om1['state'] = 'normal'
+            self.after(0, self.listbox_insert, order[1] +' '+ order[4] )
+        self.after(0, self.set_lblCustomer_text, orderList[0][1])
+        self.after(0, self.set_lblTime_text, orderList[0][2])
+        self.after(0, self.set_size_options(orderList[0][3]))
+        self.after(0, self.set_lblProduct_text, orderList[0][4])
+        return orderList
 
     
     def set_size_options(self, quality):
@@ -90,8 +108,7 @@ class MainFrame(tk.Frame):
     def option_select(self, *args):
         return self.om_variable.get()    
 
-    def set_label_text(self, text=''):
-        self.label['text'] = text
+ 
 
     def set_lblCustomer_text(self, text=''):
         self.lblCustomer['text'] = text
@@ -118,22 +135,19 @@ class MainFrame(tk.Frame):
 
     def blocking_Cancel(self):
         xml.changeStatusOnOrderXml('ocr', self.lblCustomer['text']+' '+ self.lblProduct['text'], 'Cancel')
-        print('blocking_Cancel')
-        self.listbox.delete(0,tk.END)
-        order = orderList = xml.getOrder()
-        for order in orderList:
-             self.after(0, self.listbox_insert, order[1] +' '+ order[4] )
+        self.xmlOrder()
     
     def blocking_Print(self):
         labelController.labelMaker(self.lblCustomer['text'],self.lblTime['text'], self.lblProduct['text'],self.option_select())
         xml.changeStatusOnOrderXml('ocr', self.lblCustomer['text']+' '+ self.lblProduct['text'], 'Done')
-        self.listbox.delete(0,tk.END)
-        orderList = xml.getOrder()
-        for order in orderList:
-            self.after(0, self.listbox_insert, order[1] +' '+ order[4] )
- 
+        self.xmlOrder()
+
+    def blocking_refresh(self):
+        self.xmlOrder()    
+
 if __name__ == '__main__':
     app = tk.Tk()
+    app.title("Move2x")
     app.geometry("650x275")
     app.resizable(width=False, height=False)
     main_frame = MainFrame()
