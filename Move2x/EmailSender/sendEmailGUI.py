f6b9d091d514a21ccca3cafc1ca693b667a8af6d
marc <70630992+Marc260399@users.noreply.github.com>
2021-11-30 13:57:34 +0100

    update to emailsender

---
+++ Move2x/EmailSender/sendEmailGUI.py
@@ -0,0 +1,56 @@
+import tkinter as tk
+from tkinter import *
+from concurrent import futures
+import time
+import os 
+from PIL import Image, ImageTk
+import sendEmail
+thread_pool_executor = futures.ThreadPoolExecutor(max_workers=1)
+ 
+class MainFrame(tk.Frame):
+ 
+    def __init__(self, *args, **kwargs):
+        btnY = 235
+
+        super().__init__(*args, **kwargs)
+
+        
+
+        sb = Scrollbar(self, orient=VERTICAL)
+        sb.pack(side=RIGHT, fill=Y)
+
+        self.listbox = tk.Listbox(self)
+        self.listbox.bind('<<ListboxSelect>>')
+        self.listbox.configure(yscrollcommand=sb.set)
+
+        sb.config(command=self.listbox.yview)
+        self.listbox.pack(side="left", padx=0, pady=0, anchor="nw", fill=tk.X,expand=True)
+
+
+        self.btnSend = tk.Button(self, text='Send', command=self.btnSend)
+        self.btnSend.place(y=btnY, x=300, anchor='s')
+
+        
+        
+
+    def btnSend(self):
+        thread_pool_executor.submit(self.blocking_Send)
+
+
+    def listbox_insert(self, item):
+        self.listbox.insert(tk.END, item)
+
+
+    def blocking_Send(self):
+        sendEmail.sendEmail("hey", "med dig")
+    
+
+if __name__ == '__main__':
+    app = tk.Tk()
+    app.title("Move2x")
+    app.geometry("650x245")
+    app.resizable(width=False, height=False)
+    main_frame = MainFrame()
+    app.mainloop()
+
+
