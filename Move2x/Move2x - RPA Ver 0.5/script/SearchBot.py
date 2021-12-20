dd61d399fbb4458fc9405be2953ed84782d1fb6a
ThomasHenriksen <1083787@ucn.dk>
2021-12-20 09:47:27 +0100

    Update SearchBot.py

--- Move2x/Move2x - RPA Ver 0.5/script/SearchBot.py
+++ Move2x/Move2x - RPA Ver 0.5/script/SearchBot.py
@@ -1,15 +1,38 @@
+"""
+This script is used for find, search and cropping 
+
+
+Made by 
+@BJARKE ROBERT STØVE ANDERSØN
+@CHRISTIAN MARC JØRGENSEN
+@MAGNUS SØRENSEN 
+@THOMAS HENRIKSEN  
+"""
+
+
 import numpy as np
 import cv2 
 import pyautogui as pg
 import pyautogui, time
 from PIL import Image as imgTesting
              
+"""
+This method is used for finding the coordinates 
 
+Parameters:
+
+-take_screenshot (--take_screenshot): input the name of the image 
+-Label (--Label): input the name of that its trying to find 
+Return: 
+
+-max_loc (--float): output coordinates where it fundet a match 
+"""
 def find_label(take_screenshot, Label): 
     tempSearch = 'resources\\'
     typeSearch = '.png'
     nameSearch = Label
     pathSearch = tempSearch + nameSearch + typeSearch 
+
     temp = 'temp\\'
     type = '.png'
     path = temp + take_screenshot + type 
@@ -20,12 +43,12 @@ def find_label(take_screenshot, Label):
 
     min_val, max_val, min_loc, max_loc = cv2.minMaxLoc(result)
 
-    #print('Best match top left positopn: %s' % str(max_loc))
-    print('Best match confidence: %s' % str(max_val))
+    # print('Best match top left positopn: %s' % str(max_loc))
+    # print('Best match confidence: %s' % str(max_val))
     # Sets the level off accepteable match with in screenshot
     threshold = 0.70
     if(max_val >= threshold):
-        #print(Label + ' is foundt')
+       
 
     #get dimensions of the needle image
         needle_w = needle_img.shape[1]
@@ -35,33 +58,48 @@ def find_label(take_screenshot, Label):
         top_left = max_loc
         bottom_right = (top_left[0] + needle_w, top_left[1] + needle_h)
         return max_loc
+"""
+This method is used for finding and cropping a image by finding the corners of the order 
 
+
+Return: 
+
+-listOfFiles (--string): output the names of the files, that it have croppet out of the image 
+"""
 def searchForAutoCrop(): 
+    """
+    Taking in the top corner of a order
+    """
     topCornerResour = 'resources\\'
     topCornerType = '.png'
     topCornerSearch = 'topCorner'
     topCornerpath = topCornerResour + topCornerSearch + topCornerType 
+    """
+    Taking in the bottom corner of a order
+    """
     bottomCornerResourSearch = 'resources\\'
     bottomCornerTypeSearch = '.png'
     bottomCornerNameSearch = 'bottomCorner'
     bottomCornerPathSearch = bottomCornerResourSearch + bottomCornerNameSearch + bottomCornerTypeSearch 
+    """
+    Taking in the image is being croppet
+    """
     temp = 'temp\\'
     type = '.png'
     path = temp + 'webcam' + type 
 
+    #Object Detection
     method = cv2.TM_CCOEFF_NORMED
 
     img = cv2.imread(path) #Screenshot
-    height, width, __ = img.shape
-    #haystack_img = cv2.resize(haystack_img, (1024, 720))
+    height, width, __ = img.shape #getting the size of the image 
+    
     topCorner_img = cv2.imread(topCornerpath)#Label img to find with in screenshot
     bottomCorner_img = cv2.imread(bottomCornerPathSearch)#Label img to find with in screenshot
     topCornerResult = cv2.matchTemplate(img, topCorner_img, method) # match Screenshot and Label img 
     bottomCornerResult = cv2.matchTemplate(img, bottomCorner_img, method) # match Screenshot and Label img 
-    min_val, max_val, min_loc, max_loc = cv2.minMaxLoc(topCornerResult)
-    min_val, max_val, min_loc, max_loc = cv2.minMaxLoc(bottomCornerResult)
-    #print('Best match top left positopn: %s' % str(max_loc))
-    #print('Best match confidence: %s' % str(max_val))
+
+
     # Sets the level off accepteable match with in screenshot
     bottomCorner_w = bottomCorner_img.shape[1]
     bottomCorner_h = bottomCorner_img.shape[0]
@@ -107,6 +145,17 @@ def searchForAutoCrop():
     
     return listOfFiles
 
+"""
+This method is used for search for the best match and returns a value in % (0.60)
+
+Parameters:
+
+-take_screenshot (--take_screenshot): input the name of the image 
+-whatToCheck (--whatToCheck): input the name of that its trying to find 
+Return: 
+
+-max_val (--float): output best match confidence in % 
+"""
    
         
 def check(take_screenshot, whatToCheck): 
